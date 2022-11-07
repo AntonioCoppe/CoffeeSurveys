fe9b2930d9ed7a23a87145ee584f85b8b481153f
Antonio Coppe <antonio.coppe@gmail.com>
2022-11-07 10:15:58 +0100

    switch statment

--- Program.cs
+++ Program.cs
@@ -7,11 +7,16 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
+            GenerateTaskReport();
 
+        }
+
+        public static void GenerateTaskReport()
+        {
             var task = new List<string>();
 
             // Calculated Values
-            double  responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
             double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.PriceScore + Q1Results.FoodScore) / 4;
 
             if (Q1Results.CoffeeScore < Q1Results.FoodScore)
@@ -27,7 +32,7 @@ namespace WiredBrainCoffeeSurveys.Reports
                 task.Add("Work with employees to improve overall score.");
             }
 
-            if(responseRate < .33)
+            if (responseRate < .33)
             {
                 task.Add("Reserch options for improving response rate.");
             }
@@ -40,6 +45,21 @@ namespace WiredBrainCoffeeSurveys.Reports
                 task.Add("Reward partecipant with a free coffee.");
             }
 
+            switch (Q1Results.AreaToImprove)
+            {
+                case "RewardsProgram":
+                    task.Add("Revist the rewards deals.");
+                    break;
+                case "Cleanliness":
+                    task.Add("Contact the cleaning service.");
+                    break;
+                case "MobileApp":
+                    task.Add("Contact consulting firm.");
+                    break;
+                default:
+                    task.Add("Contact the CEO.");
+                    break;
+            }
         }
     }
 }
