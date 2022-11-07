2b47cdeff8df1a11ff2f08c410a98ea3082bba48
Antonio Coppe <antonio.coppe@gmail.com>
2022-11-07 11:09:56 +0100

    Controlling Program Flow with Iteration Statments Fixes #1
    Resolves Controlling Program Flow with Iteration Statments #1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WiredBrainCoffeeSurveys.Reports
 {
@@ -7,59 +8,120 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
-            GenerateTaskReport();
+            GenerateWinnerEmails();
 
+            GenerateTasksReport();
+
+            GenerateCommentsReport();
         }
 
-        public static void GenerateTaskReport()
+        public static void GenerateWinnerEmails()
         {
-            var task = new List<string>();
+            var selectedEmails = new List<string>();
+            int counter = 0;
 
-            // Calculated Values
+            Console.WriteLine(Environment.NewLine + "Selected Winners Output:");
+            while (selectedEmails.Count < 2 && counter < Q1Results.Responses.Count)
+            {
+                var currentItem = Q1Results.Responses[counter];
+
+                if (currentItem.FavoriteProduct == Q1Results.FavoriteProduct)
+                {
+                    selectedEmails.Add(currentItem.EmailAddress);
+                    Console.WriteLine(currentItem.EmailAddress);
+                }
+
+                counter++;
+            }
+
+            File.WriteAllLines("WinnersReport.csv", selectedEmails);
+        }
+
+        public static void GenerateCommentsReport()
+        {
+            var comments = new List<string>();
+
+            Console.WriteLine(Environment.NewLine + "Comments Output:");
+            for (var i = 0; i < Q1Results.Responses.Count; i++)
+            {
+                var currentResponse = Q1Results.Responses[i];
+
+                if (currentResponse.WouldRecommend < 7.0)
+                {
+                    Console.WriteLine(currentResponse.Comments);
+                    comments.Add(currentResponse.Comments);
+                }
+            }
+
+            foreach (var response in Q1Results.Responses)
+            {
+                if (response.AreaToImprove == Q1Results.AreaToImprove)
+                {
+                    Console.WriteLine(response.Comments);
+                    comments.Add(response.Comments);
+                }
+            }
+
+            File.WriteAllLines("CommentsReport.csv", comments);
+        }
+
+        public static void GenerateTasksReport()
+        {
+            var tasks = new List<string>();
+
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
-            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.PriceScore + Q1Results.FoodScore) / 4;
+            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
 
             if (Q1Results.CoffeeScore < Q1Results.FoodScore)
             {
-                task.Add("Investigate why coffee score is lower than food score.");
+                tasks.Add("Investigate coffee recipes and ingredients.");
             }
+
             if (overallScore > 8.0)
             {
-                task.Add("Work with leadership to understand what is driving the high overall score.");
+                tasks.Add("Work with leadership to reward staff");
             }
             else
             {
-                task.Add("Work with employees to improve overall score.");
+                tasks.Add("Work with employees for improvement ideas.");
             }
 
             if (responseRate < .33)
             {
-                task.Add("Reserch options for improving response rate.");
+                tasks.Add("Research options to improve response rate.");
             }
             else if (responseRate > .33 && responseRate < .66)
             {
-                task.Add("Reward partecipant with a discount coupon.");
+                tasks.Add("Reward participants with free coffee coupon.");
             }
             else
             {
-                task.Add("Reward partecipant with a free coffee.");
+                tasks.Add("Rewards participants with discount coffee coupon.");
             }
 
             switch (Q1Results.AreaToImprove)
             {
-                case "RewardsProgram":
-                    task.Add("Revist the rewards deals.");
+                case  "RewardsProgram":
+                    tasks.Add("Revisit the rewards deals.");
                     break;
                 case "Cleanliness":
-                    task.Add("Contact the cleaning service.");
+                    tasks.Add("Contact the cleaning vendor.");
                     break;
                 case "MobileApp":
-                    task.Add("Contact consulting firm.");
+                    tasks.Add("Contact the consulting firm about app.");
                     break;
                 default:
-                    task.Add("Contact the CEO.");
+                    tasks.Add("Investigate individual comments for ideas.");
                     break;
             }
+
+            Console.WriteLine(Environment.NewLine + "Tasks Output:");
+            foreach(var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            File.WriteAllLines("TasksReport.csv", tasks);
         }
     }
 }
