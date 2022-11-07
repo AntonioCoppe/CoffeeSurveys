f948d8d226d7c09add7c77e9078cd925c934fffb
Antonio Coppe <antonio.coppe@gmail.com>
2022-11-07 09:52:22 +0100

    'Complex Decision statments'

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WiredBrainCoffeeSurveys.Reports
 {
@@ -6,24 +7,38 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
+
+            var task = new List<string>();
+
             // Calculated Values
             double  responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
-            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
             double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.PriceScore + Q1Results.FoodScore) / 4;
 
-            Console.WriteLine($"Reseponse Percentage: {responseRate}");
-            Console.WriteLine($"UnansweredCount Surveys: {unansweredCount}");
-            Console.WriteLine($"Overall Score: {overallScore}");
+            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
+            {
+                task.Add("Investigate why coffee score is lower than food score.");
+            }
+            if (overallScore > 8.0)
+            {
+                task.Add("Work with leadership to understand what is driving the high overall score.");
+            }
+            else
+            {
+                task.Add("Work with employees to improve overall score.");
+            }
 
-            // Conditional Statements
-            bool higherCoffeeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
-            bool customerRecommend = Q1Results.WouldRecommend >= 7;
-            bool noGranolaYesCappuccino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappuccino";
-
-            // Display the results
-            Console.WriteLine($"Higher Coffee Score: {higherCoffeeScore}");
-            Console.WriteLine($"Customer Recommend: {customerRecommend}");
-            Console.WriteLine($"Hate Granola Love Cappuccino: {noGranolaYesCappuccino}");            
+            if(responseRate < .33)
+            {
+                task.Add("Reserch options for improving response rate.");
+            }
+            else if (responseRate > .33 && responseRate < .66)
+            {
+                task.Add("Reward partecipant with a discount coupon.");
+            }
+            else
+            {
+                task.Add("Reward partecipant with a free coffee.");
+            }
 
         }
     }
